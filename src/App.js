c7fe6a0a7e57b79eab3a13fd6e96c31e0a06859d
lnabua <nabua.lorraine@gmail.com>
2021-06-20 17:43:09 -0700

    css update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Software Engineer</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
